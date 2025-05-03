f42c8acfe3ce23ea2fa57b68571f0ea40b66ec7c
Musharraf777 <musharrafsayyad1@gmail.com>
2025-05-03 14:47:17 +0530

    test the APIS - get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const app = express();
 
+// this will only handle get Calls to /user
+app.get("/user",(req,res)=>{
+    res.send({fName : "Syed", lName : "Musharraf"})
+})
 
+// post
+app.post("/savedUser",(req, res)=>{
+    res.send("User Saved SuccessFully to the DataBase (MongoDB..)")
+    console.log(res.body)
+})
+
+// delete
+app.delete("/delete",(req, res)=>{
+    res.send("User Deleted Successfully...")
+})
+
+
+// this will match all api the HTTP method API call to /hello
 app.use("/hello",(req, res)=>{
     res.send("My name is Hello kiujy")
-})
-app.use("/text",(req, res)=>{
+}) 
+app.use("/hello/mini",(req, res)=>{
     res.send("My name is Khan")
 })
+
+
+
 
 
 app.listen(9999,()=>{
